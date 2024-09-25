226d526dcaf0e08fecd3f922d2fce364aa888ccc
TommyEm <tommyholyshit@gmail.com>
2024-09-25 11:10:52 +0200

    Attempt to fix touch device gestures

--- src/index.jsx
+++ src/index.jsx
@@ -1,20 +1,33 @@
-import "./style.css";
 import ReactDOM from "react-dom/client";
 import { Canvas } from "@react-three/fiber";
+import { KeyboardControls } from "@react-three/drei";
 import Experience from "./Experience.jsx";
+
+import "./style.css";
 
 const root = ReactDOM.createRoot(document.querySelector("#root"));
 
 root.render(
-  <Canvas
-    shadows
-    camera={{
-      fov: 45,
-      near: 0.1,
-      far: 200,
-      position: [-4, 3, 6],
-    }}
+  <KeyboardControls
+    map={[
+      { name: "forward", keys: ["ArrowUp", "KeyW"] },
+      { name: "backward", keys: ["ArrowDown", "KeyS"] },
+      { name: "left", keys: ["ArrowLeft", "KeyA"] },
+      { name: "right", keys: ["ArrowRight", "KeyD"] },
+      { name: "jump", keys: ["Space"] },
+    ]}
   >
-    <Experience />
-  </Canvas>
+    <Canvas
+      className="r3f"
+      shadows
+      camera={{
+        fov: 45,
+        near: 0.1,
+        far: 200,
+        position: [-4, 3, 6],
+      }}
+    >
+      <Experience />
+    </Canvas>
+  </KeyboardControls>
 );
